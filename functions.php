b7c20eca8da11ae4b74cba80d1dd63e8ef2b38a9
Anraiki <anraiki@gmail.com>
2016-09-12 14:05:46 -0700

    Condense the code to be more efficient without using the 'wpdb' class to query and find custom post. But to find registered custom posts.

--- functions.php
+++ functions.php
@@ -2,33 +2,23 @@
 
 //User friendly implementation of having all post type shown in the archives
 function custom_post_in_archive($query){
-    if(is_archive()) {
-        global $wpdb;
+    if($query->is_archive) {
 
-        //Post Types of Wordpress
-        $wordPress_postType = array(
-            'post',
-            'page',
-            'attachment',
-            'revision',
-            'nav_menu_item'
+        //By setting '_builtin' to false, we exclude the WordPress built-in public post types.
+        $args = array(
+            'public'   => true,
+            '_builtin' => false
         );
 
-        //A SQL Query to grab the post types.
-        $cptQuery = "Select `post_type` FROM `" . $wpdb->prefix . "posts` GROUP BY `post_type`";
-        $results = $wpdb->get_results($cptQuery);
+        //Get all Custom Post Types
+        $customPostType = get_post_types( $args );
 
-        //We are adding to read "Posts" and "Custom Post Type".
+        //Also add "Posts" to be in the loop.
         $customPostType[] = "post";
-
-        //Ensure we grab a custom post type by comparing it to WordPress's post types
-        foreach($results as $key => $result) {
-            if( !in_array($result->post_type, $wordPress_postType) )
-                $customPostType[] = $result->post_type;
-        }
 
         //Make the Query
         $query->set( 'post_type', $customPostType );
+
     }
 }
 add_action( 'pre_get_posts', 'custom_post_in_archive' );
